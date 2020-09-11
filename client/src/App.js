7546614b8c9e9735af5b77fbabb2e2aa614d76ec
asamaiya00 <asamaiya00@gmail.com>
2020-09-11 16:30:21 +0530

    Launch page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import logo from './logo.png';
 import Launches from './components/Launches';
-
+import Launch from './components/Launch';
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
 });
@@ -12,14 +13,17 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="container">
-        <img
-          src={logo}
-          alt="SpaceX"
-          style={{ width: 500, margin: 'auto', display: 'flex' }}
-        />
-        <Launches />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <img
+            src={logo}
+            alt="SpaceX"
+            style={{ width: 500, margin: 'auto', display: 'flex' }}
+          />
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
